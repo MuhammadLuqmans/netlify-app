4f3fc6c8bd71f0e222a080f636c5f46a36a9d3e5
MuhammadLuqmans <musa60902@gmail.com>
2022-01-02 16:47:19 +0500

    reupdate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-
+import "./Styles/index.css"
 
 export default function Home() {
   return(
-     <>
+     <div className="Container">
      <h3>Hello World <br /> Muhammad luqman</h3>
-     </>
+     </div>
   )}
